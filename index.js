50dbb0bf8df0fda16aea112635de053b9cc81573
Distartin Studio <91096705+distartin@users.noreply.github.com>
2023-12-15 13:08:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 // index.js
+const express = require('express');
 const screenshotModule = require('./screenshot');
 const videoModule = require('./video');
 
-async function run() {
-  await screenshotModule.captureScreenshot();
-  videoModule.createVideo();
-}
+const app = express();
+const port = process.env.PORT || 3000;
 
-run();
+app.get('/generate-video', async (req, res) => {
+  try {
+    await screenshotModule.captureScreenshot();
+    videoModule.createVideo();
+    res.status(200).json({ message: 'Video generation initiated successfully.' });
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
