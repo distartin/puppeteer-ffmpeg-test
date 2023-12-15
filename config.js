0f10f9fe50d0107b365b4944a9938e6a0dc1cf3d
Distartin Studio <91096705+distartin@users.noreply.github.com>
2023-12-15 17:25:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 
 module.exports = {
-  screenshotsFolder: path.join(__dirname, 'screenshots'),
-  videoOutputPath: path.join(__dirname, 'output.mp4'),
+  screenshotsFolder: path.join(__dirname, 'public', 'screenshots'), // Updated path for screenshots
+  videoOutputPath: path.join(__dirname, 'public', 'output.mp4'), // Updated path for video output
   apiPort: process.env.PORT || 3000,
 };
